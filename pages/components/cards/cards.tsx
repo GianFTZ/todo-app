82254f17d8bf60e5b8d71472caeb038448b4af6d
GianFTZ <gianfelipetz@hotmail.com>
2022-07-07 10:45:17 -0300

    svgs add again

--- pages/components/cards/cards.tsx
+++ pages/components/cards/cards.tsx
@@ -41,7 +41,7 @@ export const Card_Component = ({TO_DO_LIST}: CardProps) => {
                     <WriteSVG />
                   </span>
                   <span onClick={()=>{ setID(index), handleDeleteClick()}}>
-                  
+                  <svg  xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
                   </span>
                 </div>
                 <span className="absolute -left-3 -top-3 bg-green-500 flex justify-center items-center rounded-full w-8 h-8 text-gray-50 font-bold">
